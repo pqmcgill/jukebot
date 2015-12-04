42ceffbda9cdb82aef24f18c5113d694d520f8e1
pqmcgill <Gocolts90!>
2015-12-03 22:48:40 -0600

    test

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],{0:function(e,n,t){"use strict";var r=t(86),l=t(87),c=r.createClass({displayName:"App",render:function(){return r.createElement("div",null,"Hello, World!")}});l.render(r.createElement(c,null),document.body)},87:function(e,n,t){"use strict";e.exports=t(58)}});
+webpackJsonp([1],{0:function(e,t,n){"use strict";var r=n(86),l=n(87),c=r.createClass({displayName:"App",render:function(){return r.createElement("div",null,"Hello, World!")}});l.render(r.createElement(c,null),document.getElementById("app"))},87:function(e,t,n){"use strict";e.exports=n(58)}});
