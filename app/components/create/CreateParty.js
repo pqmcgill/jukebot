523853d146da26ed770c67e6c92f5b92f0fa9869
pqmcgill <pqmcgill@gmail.com>
2016-02-21 16:45:18 -0600

    setup track loop experiment

--- app/components/create/CreateParty.js
+++ app/components/create/CreateParty.js
@@ -4,72 +4,23 @@ let SongSearch = require('../inParty/search/SongSearch');
 let rhapsodyUtil = require('../../util/rhapsodyUtil');
 
 let CreateParty = React.createClass({
-  //getInitialState () {
-  //  return {
-  //    verifierCode: null,
-  //    partyStarted: false
-  //  }
-  //},
-  //  componentWillMount () {
-  //    // grab verifier token from URL
-  //    if (window.location.search.match(/^\?code=*/)) { 
-  //      console.log('foo');
-  //      var query = window.location.search.substring(1);
-  //      var vars = query.split('&');
-  //      var state = {};
-  //      // store code and state params into an object
-  //      vars.forEach((v, i) => {
-  //        var pair = v.split('=');
-  //        if (pair[0] === 'code') { state.code = pair[1]; }
-  //        if (pair[0] === 'state') { state.state = pair[1]; }
-  //      });
-  //      // build a new url
-  //      console.log(state);
-  //      var { origin, hash } = window.location;
-  //      // replace state without query params, but maintain state through
-  //      // HTML5 history object
-  //      window.history.replaceState(state, '', [origin, hash].join(''));
-  //    }
-  //  },
 
-//  componentDidMount () {
-//    // set code to that in the history object if it exists
-//    if (window.history.state) {
-//      this.setState({
-//        code: window.history.state.code,
-//        continueAuth: window.history.state.state === 'continueAuth'
-//      });
-//    }
-//  },
+  tracks: [
+    'Tra.2169032',
+    'Tra.7289598',
+    'Tra.5156525',
+    'Tra.5156526',
+    'Tra.202338320',
+    'Tra.3310729',
+    'Tra.5156522',
+    'Tra.30638627',
+    'Tra.20950996',
+    'Tra.7289599'
+  ],
 
-//  componentDidUpdate () {
-//    if (this.state.code && this.state.continueAuth) {
-//      this.createParty();
-//      this.setState({
-//        continueAuth: false
-//      });
-//    } 
-//  },
-
-
-
-//  // test authenticating with rhapsody and starting player
-//  createParty (e) {
-//    if (e) { e.preventDefault(); }
-//    rhapsodyUtil.authenticate(this.state.code,  (err, res) => {
-//      if (err) {
-//        console.log('there was an error logging in...', err);
-//      } else {
-//        console.log('logged in...', res);
-//        this.setState({
-//          partyStarted: true
-//        });
-//        firebaseUtil.createParty();
-//      }
-//    });
-//  },
-
-//  // play a track: Tra.5156528 = "Say it ain't so" by Weezer :)
+  playTracks () {
+  
+  },
 
   render () {
     return (
