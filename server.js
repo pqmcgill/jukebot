e1a79826af2b0b776de2b4aa36574989da1e3262
pqmcgill <pqmcgill@gmail.com>
2016-03-01 16:32:25 -0600

    simplified server.js

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
 var express = require('express');
 var path = require('path');
-var webpack = require('webpack');
 var app = express();
 
-var isDevelopment = (process.env.NODE_ENV !== 'production');
-var prod_path = path.join(__dirname, 'dist');
-var dev_path = path.join(__dirname, 'public');
+var static_path = path.join(__dirname, 'dist');
 
-app.use(express.static(prod_path))
-  .get('*', function(req, res) {
-    res.sendFile('index.html', {
-      root: prod_path
-    });
-  }).listen(process.env.PORT || 8080, function(err) {
-    if (err) { console.log(err); }
-    console.log('Listening at localhost:8080');
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(static_path));
+
+// pushstate configuration!!!
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {
+    root: static_path
   });
+});
 
-  //if (isDevelopment) {
-  //  var config = require('./webpack.config');
-  //  var WebpackDevServer = require('webpack-dev-server');
-  //
-  //  new WebpackDevServer(webpack(config), {
-  //    publicPath: '/',
-  //    path: dev_path,
-  //    hot: true,
-  //    historyApifallback: true
-  //  }).listen(3000, 'localhost', function(err, result) {
-  //    if (err) { console.log(err); }
-  //    console.log('Listening at localhost:3000');
-  //  });
-  //}
+// define api calls eventually...
+
+app.listen(PORT, function(err) {
+  if (err) { console.log(err); }
+  console.log('Listening on port:', PORT);
+});
+
