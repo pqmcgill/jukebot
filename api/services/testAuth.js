ec03194fb1982dbc8a2f5af8d2eee1da4d5cef20
pqmcgill <pqmcgill@gmail.com>
2016-03-03 09:56:15 -0600

    moved APP_SECRET to process.env

--- api/services/testAuth.js
+++ api/services/testAuth.js
@@ -3,7 +3,7 @@ var Firebase = require('firebase');
 // A test for passing firebase auth variables to backend
 module.exports = function(req, res) {
   var ref = new Firebase('http://jukebot.firebaseio.com');
-  ref.authWithCustomToken('tLSZF2uAC2zs6fyHmeh4NWXCGbjLgu8fBQhhJ7qO', function(err) {
+  ref.authWithCustomToken(process.env.APP_SECRET, function(err) {
     if (err) {
       console.log(err);
       return;
